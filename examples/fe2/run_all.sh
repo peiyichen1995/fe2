d32b798415b341a619628b30515dd0ceb9c00aff
peiyi <peiyic03@gmail.com>
2023-01-17 09:50:49 -0500

    update run_all to 1000 samples

--- examples/fe2/run_all.sh
+++ examples/fe2/run_all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 COUNTER=0
 INLOOPCOUNTER=0
-for L in {0..1}; do
-  ../../fe2-opt -i gamma_multiapp_square.i n=2 n_micro=2 sample=$L 2> err_output/$L.txt > command_output/$L.txt &
+for L in {0..999}; do
+  ../../fe2-opt -i gamma_multiapp_square.i n=8 n_micro=8 sample=$L 2> err_output/$L.txt > command_output/$L.txt &
   (( COUNTER ++ ))
-  if [ $COUNTER = 2 ]; then
+  if [ $COUNTER = 16 ]; then
     wait
     COUNTER=0
     (( INLOOPCOUNTER ++ ))
